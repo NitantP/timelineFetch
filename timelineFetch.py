3e5f09ea4cc79cf4f1a3fe5162ab5b099c70ed57
Nitant <patel.nitant.bca@gmail.com>
2017-07-13 17:50:10 -0400

    Trying out configparser to hide OAuth keys for twitter API

--- timelineFetch.py
+++ timelineFetch.py
@@ -1,5 +1,16 @@
+import configparser
 import tweepy
 from tweepy import OAuthHandler
+
+config = configparser.ConfigParser()
+
+config.read('./../config.ini')
+
+if 'api.twitter.com' in config:
+	consumer_key = config['api.twitter.com']['consumer_key']
+	consumer_secret = config['api.twitter.com']['consumer_secret']
+	access_token = config['api.twitter.com']['access_token']
+	access_secret = config['api.twitter.com']['access_secret']
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
