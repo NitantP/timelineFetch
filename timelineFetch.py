13e182c89bc15a61cfb114feb752de102fc2cdbb
Nitant <patel.nitant.bca@gmail.com>
2017-07-13 18:34:26 -0400

    Added small UI to take user input and send request using Fetch button or <ENTER>

--- timelineFetch.py
+++ timelineFetch.py
@@ -1,25 +1,41 @@
 import configparser
+import tkinter as tk
 import tweepy
 from tweepy import OAuthHandler
 
-config = configparser.ConfigParser()
 
-config.read('./../config.ini')
+master = tk.Tk()
 
-if 'api.twitter.com' in config:
-	consumer_key = config['api.twitter.com']['consumer_key']
-	consumer_secret = config['api.twitter.com']['consumer_secret']
-	access_token = config['api.twitter.com']['access_token']
-	access_secret = config['api.twitter.com']['access_secret']
+def fetchtimeline(event=None):
+	config = configparser.ConfigParser()
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+	config.read('../config.ini')
 
-api = tweepy.API(auth)
+	if 'api.twitter.com' in config:
+		consumer_key = config['api.twitter.com']['consumer_key']
+		consumer_secret = config['api.twitter.com']['consumer_secret']
+		access_token = config['api.twitter.com']['access_token']
+		access_secret = config['api.twitter.com']['access_secret']
 
-twitterid = input('ID of timeline you want to retrieve: ')
-print('------------------------------')
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_secret)
 
-for status in tweepy.Cursor(api.user_timeline, id=twitterid).items(10):
-	print(status.text)
-	print('------------------------------')
+	api = tweepy.API(auth)
+
+	twitterid = inputid.get()
+
+	for status in tweepy.Cursor(api.user_timeline, id=twitterid).items(10):
+		print(status.text)
+		print('------------------------------')
+
+tk.Label(master, text='TwitterID').grid(row=0)
+
+inputid = tk.StringVar()
+e1 = tk.Entry(master, textvariable=inputid)
+e1.grid(row=0, column=1)
+e1.bind('<Return>', fetchtimeline)
+e1.focus_set()
+
+tk.Button(master, text='Fetch', command=fetchtimeline).grid(row=0, column=2)
+
+master.mainloop()
