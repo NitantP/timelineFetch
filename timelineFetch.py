65a1f90c5d86a8ade59824fdf827b6185cffbedc
Nitant <patel.nitant.bca@gmail.com>
2017-07-13 17:31:30 -0400

    Undo

--- timelineFetch.py
+++ timelineFetch.py
@@ -1,11 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = 'JjvyKzr2I8LKC0fVzNyby2zkn'
-consumer_secret = 'U8Unuae2XEp434wO2HhwyhZFi6w7XwRtK1RTPatHs5v4dulmtQ'
-access_token = '465212227-tN1DXqn5lTEUHpqJ8mD6PteiuUi9FNx2I7x4pNoO'
-access_secret = 'lwEdULHSqgUc6u2jUBm6VLuJdzKn5S57zfKfi4KJlh5mz'
-
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
